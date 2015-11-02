9529ab3bbcb3bbcfb30ec6193c5c5e26646f7007
shubaivan <shuba.ivan@yandex.ru>
2015-11-02 18:57:22 +0200

    add getContent

--- src/AppBundle/Controller/Api/UserController.php
+++ src/AppBundle/Controller/Api/UserController.php
@@ -16,7 +16,7 @@ class UserController extends Controller
      */
     public function authorizationAction(Request $request)
     {
-        dump($_POST, $request);exit;
+        dump($_POST, $content = $this->get("request")->getContent(), $request);exit;
         $data = $this->get('app.security.bank_id')->getAccessToken($request->request->get('code'));
         if ($data['state'] == 'ok') {
             $user = $this->get('app.user.manager')->isUniqueUser($data);
