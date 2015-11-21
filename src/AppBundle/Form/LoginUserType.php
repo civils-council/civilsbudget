8fdb342f081b8e77bca4d9e79bcb2a874c158806
shubaivan <shuba.ivan@yandex.ru>
2015-11-21 17:52:54 +0200

    create LoginUserType

---
+++ src/AppBundle/Form/LoginUserType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class LoginUserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstName')
+            ->add('lastName')
+            ->add('phone')
+            ->add('birthday')
+            ->add('email')
+//            ->add('secret')
+            ->setRequired(false)
+        ;
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\User'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'civils_user';
+    }
+}
