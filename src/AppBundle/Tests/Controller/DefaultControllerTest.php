97e425593ce7917de5e899f2123e1fa161aace30
shubaivan <shuba.ivan@yandex.ru>
2015-11-26 21:27:45 +0200

    test GET /projects

--- src/AppBundle/Tests/Controller/DefaultControllerTest.php
+++ src/AppBundle/Tests/Controller/DefaultControllerTest.php
@@ -10,9 +10,9 @@ class DefaultControllerTest extends WebTestCase
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/');
+        $crawler = $client->request('GET', '/projects');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+        $this->assertContains('Projects', $crawler->filter('html:contains(Projects)')->text());
     }
 }
