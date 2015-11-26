c901c1d74d763d3be1986e6ab8e42240d5c60d64
shubaivan <shuba.ivan@yandex.ru>
2015-11-26 19:48:52 +0200

    app kernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
 
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new Mcfedr\JsonFormBundle\McfedrJsonFormBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
 
             new AppBundle\AppBundle(),
         );
