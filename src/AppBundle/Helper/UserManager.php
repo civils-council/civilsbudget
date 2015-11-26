3a9c196e3020b294cb39986f723a2ddbff27ea7d
shubaivan <shuba.ivan@yandex.ru>
2015-11-26 13:03:14 +0200

    change UserManager

--- src/AppBundle/Helper/UserManager.php
+++ src/AppBundle/Helper/UserManager.php
@@ -31,42 +31,64 @@ class UserManager
         fclose($fp);
         $res = openssl_get_privatekey($pub_key);
 
+        if(array_key_exists('addresses', $data['customer']) == true) {
+            if (array_key_exists('country', $data['customer']['addresses'][0]) == true) {
+                $deceode_country = base64_decode($data['customer']['addresses'][0]['country']);
+                $result_field = openssl_private_decrypt($deceode_country, $country, $res);
+            }
 
-        $deceode_clId = base64_decode($data['customer']['clId']);
-        $result_field = openssl_private_decrypt($deceode_clId,$clId,$res);
+            if (array_key_exists('state', $data['customer']['addresses'][0]) == true) {
+                $deceode_state = base64_decode($city = $data['customer']['addresses'][0]['state']);
+                $result_field = openssl_private_decrypt($deceode_state, $state, $res);
+            }
 
-        $deceode_country = base64_decode($data['customer']['addresses'][0]['country']);
-        $result_field = openssl_private_decrypt($deceode_country,$country,$res);
+            if (array_key_exists('street', $data['customer']['addresses'][0]) == true) {
+                $deceode_street = base64_decode($data['customer']['addresses'][0]['street']);
+                $result_field = openssl_private_decrypt($deceode_street, $street, $res);
+            }
 
-        $deceode_state = base64_decode($city = $data['customer']['addresses'][0]['state']);
-        $result_field = openssl_private_decrypt($deceode_state,$state,$res);
+            if (array_key_exists('houseNo', $data['customer']['addresses'][0]) == true) {
+                $deceode_houseNo = base64_decode($data['customer']['addresses'][0]['houseNo']);
+                $result_field = openssl_private_decrypt($deceode_houseNo, $houseNo, $res);
+            }
 
-        $deceode_street = base64_decode($data['customer']['addresses'][0]['street']);
-        $result_field = openssl_private_decrypt($deceode_street,$street,$res);
+            if (array_key_exists('flatNo', $data['customer']['addresses'][0]) == true) {
+                $deceode_flatNo = base64_decode($data['customer']['addresses'][0]['flatNo']);
+                $result_field = openssl_private_decrypt($deceode_flatNo, $flatNo, $res);
+            }
 
-        $deceode_houseNo = base64_decode($data['customer']['addresses'][0]['houseNo']);
-        $result_field = openssl_private_decrypt($deceode_houseNo,$houseNo,$res);
+            if (array_key_exists('city', $data['customer']['addresses'][0]) == true) {
+                $deceode_city = base64_decode($data['customer']['addresses'][0]['city']);
+                $result_field = openssl_private_decrypt($deceode_city, $city, $res);
+            }
+        }
 
-        $deceode_flatNo = base64_decode($data['customer']['addresses'][0]['flatNo']);
-        $result_field = openssl_private_decrypt($deceode_flatNo,$flatNo,$res);
+        if(array_key_exists('clId', $data['customer']) == true) {
+            $deceode_clId = base64_decode($data['customer']['clId']);
+            $result_field = openssl_private_decrypt($deceode_clId, $clId, $res);
+        }
 
-        $deceode_city = base64_decode($data['customer']['addresses'][0]['city']);
-        $result_field = openssl_private_decrypt($deceode_city,$city,$res);
+        if(array_key_exists('firstName', $data['customer']) == true) {
+            $deceode_firstName = base64_decode($data['customer']['firstName']);
+            $result_field = openssl_private_decrypt($deceode_firstName, $firstName, $res);
+        }
 
+        if(array_key_exists('lastName', $data['customer']) == true) {
+            $deceode_lastName = base64_decode($data['customer']['lastName']);
+            $result_field = openssl_private_decrypt($deceode_lastName, $lastName, $res);
+        }
 
-        $deceode_firstName = base64_decode($data['customer']['firstName']);
-        $result_field = openssl_private_decrypt($deceode_firstName,$firstName,$res);
+        if(array_key_exists('middleName', $data['customer']) == true) {
+            $deceode_middleName = base64_decode($data['customer']['middleName']);
+            $result_field = openssl_private_decrypt($deceode_middleName, $middleName, $res);
+        }
 
-        $deceode_lastName = base64_decode($data['customer']['lastName']);
-        $result_field = openssl_private_decrypt($deceode_lastName,$lastName,$res);
+        if(array_key_exists('sex', $data['customer']) == true) {
+            $deceode_sex = base64_decode($data['customer']['sex']);
+            $result_field = openssl_private_decrypt($deceode_sex, $sex, $res);
+        }
 
-        $deceode_middleName = base64_decode($data['customer']['middleName']);
-        $result_field = openssl_private_decrypt($deceode_middleName,$middleName,$res);
-
-        $deceode_sex = base64_decode($data['customer']['sex']);
-        $result_field = openssl_private_decrypt($deceode_sex,$sex,$res);
-
-        $user = $this->em->getRepository('AppBundle:User')->findByClid($clId);
+        $user = $this->em->getRepository('AppBundle:User')->findOneByClid($clId);
         if (empty($user)) {
 
             $location = $this->em->getRepository('AppBundle:Location')->findOneByCity($city);
@@ -74,8 +96,11 @@ class UserManager
                 $location = new Location();
             }
             $location
-                ->setCity($city)
-                ->setAddress($street . ',' . $houseNo . 'appartment' . $flatNo)
+                ->setCity($city);
+                if(!empty($flatNo)) {
+                    $location->setAddress($street . ',' . $houseNo . 'appartment' . $flatNo);
+                        };
+            $location
                 ->setCountry($country)
                 ->setDistrict($city)
                 ;
@@ -91,11 +116,13 @@ class UserManager
 
             $this->em->persist($user);
             $this->em->flush();
+
+            return [$user, 'new'];
         }
         else
         {
             //ToDo add user in security context
+            return [$user, 'old'];
         }
-        return $user;
     }
 }
