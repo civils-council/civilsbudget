1d84489c6af5e109d0602964d790bb7702b6ab20
spolischook <spolischook@gmail.com>
2015-11-30 00:27:48 +0200

    Removed unused code

--- src/AppBundle/Exception/ClientIdBadRequestException.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace AppBundle\Exception;
-
-use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
-
-class ClientIdBadRequestException extends BadRequestHttpException
-{
-
-}
