706d03a9fd94e1a35040aec0bdf318c5760d08d5
spolischook <spolischook@gmail.com>
2015-12-02 18:27:02 +0200

    Added encryptory functionality

--- src/AppBundle/DependencyInjection/Configuration.php
+++ src/AppBundle/DependencyInjection/Configuration.php
@@ -20,9 +20,12 @@ class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('app');
 
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
+        $rootNode
+            ->children()
+                ->scalarNode('public_key')->end()
+                ->scalarNode('private_key')->end()
+            ->end()
+        ;
 
         return $treeBuilder;
     }
