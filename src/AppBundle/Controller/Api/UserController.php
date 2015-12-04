c4d30b66ee75f13a63ca93a0720aea7f5aa6fdad
shubaivan <shuba.ivan@yandex.ru>
2015-12-04 18:32:05 +0200

    add /api/login action

--- src/AppBundle/Controller/Api/UserController.php
+++ src/AppBundle/Controller/Api/UserController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller\Api;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -39,4 +40,41 @@ class UserController extends Controller
         }
         return new JsonResponse(["code:" => 401, "message" => "Wrong authorization."]);
     }
+
+    /**
+     * @Route("/api/login", name="api_login")
+     * @Method({"GET", "POST"})
+     */
+    public function loginAction(Request $request)
+    {
+        $code = $request->query->get('code');
+
+        if ($code = $request->query->get('code')) {
+            $accessToken = $this->get('app.security.bank_id')->getAccessToken($code);
+            $data = $this->get('app.security.bank_id')->getBankIdUser($accessToken['access_token']);
+            if ($data['state'] == 'ok') {
+                $user = $this->get('app.user.manager')->isUniqueUser($data);
+
+                $vote = $user[0]->getLikedProjects();
+                if(!is_null($vote)){
+                    $voted_project = $user[0]->getLikedProjects()->getId();
+                }else{
+                    $voted_project = false;
+                }
+                return new JsonResponse(
+                    [
+                        "id" => $user[0]->getId(),
+                        "full_name" => $user[0]->getFullName(),
+                        "clid" => $user[0]->getClid(),
+                        "voted_project" => $voted_project
+                    ]
+                );
+            }
+        }else{
+            throw new NotFoundHttpException('No find code in request');
+        }
+
+    }
+
+
 }
