e463632be9cec87d39eddf92cb3d30addfce1d23
shubaivan <shuba.ivan@yandex.ru>
2015-12-07 22:30:10 +0200

    getProject order count like only city Che

--- src/AppBundle/Entity/ProjectRepository.php
+++ src/AppBundle/Entity/ProjectRepository.php
@@ -10,4 +10,25 @@ namespace AppBundle\Entity;
  */
 class ProjectRepository extends \Doctrine\ORM\EntityRepository
 {
+    public function getProject()
+    {
+        $qb = $this->getEntityManager()->createQueryBuilder('d');
+        $qb
+            ->select('d')
+            ->from('AppBundle:Project', 'd')
+            ->addSelect('COUNT(m.id) as nMethods')
+            ->join('d.likedUsers', 'm')
+            ->join('m.location', 'l')
+            ->where('l.city = :identifier')
+            ->setParameter('identifier', 'ЧЕРКАСИ')
+            ->groupBy('d.id')
+            ->orderBy("nMethods", 'DESC')
+
+            ->getQuery()
+            ->getResult()
+        ;
+        $query = $qb->getQuery();
+        $results = $query->getResult();
+        return $results;
+    }
 }
