370274591086c79f58f5287b2d835bed783bcda1
shubaivan <shuba.ivan@yandex.ru>
2015-12-08 12:40:00 +0200

    create getOneProjectShow

--- src/AppBundle/Entity/ProjectRepository.php
+++ src/AppBundle/Entity/ProjectRepository.php
@@ -37,11 +37,36 @@ class ProjectRepository extends \Doctrine\ORM\EntityRepository
         $qb
             ->select('d')
             ->from('AppBundle:Project', 'd')
+            ->addSelect('COUNT(m.id) as nMethods')
             ->join('d.likedUsers', 'm')
             ->join('m.location', 'l')
             ->where('d.confirm = :confirm')
             ->andWhere($qb->expr()->like('l.city', $qb->expr()->literal('%ЧЕРКАС%')))
             ->setParameter('confirm', 'approved')
+            ->groupBy('d.id')
+
+            ->getQuery()
+            ->getResult()
+        ;
+        $query = $qb->getQuery();
+        $results = $query->getResult();
+        return $results;
+    }
+
+    public function getOneProjectShow($id)
+    {
+        $qb = $this->getEntityManager()->createQueryBuilder('d');
+        $qb
+            ->select('d')
+            ->from('AppBundle:Project', 'd')
+            ->addSelect('COUNT(m.id) as nMethods')
+            ->join('d.likedUsers', 'm')
+            ->join('m.location', 'l')
+            ->where('d.confirm = :confirm')
+            ->andWhere('d.id = :id')
+            ->andWhere($qb->expr()->like('l.city', $qb->expr()->literal('%ЧЕРКАС%')))
+            ->setParameter('confirm', 'approved')
+            ->setParameter('id', $id)
             ->groupBy('d.id')
 
             ->getQuery()
