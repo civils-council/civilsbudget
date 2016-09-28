ef911737d8f07b9f3ac20ca2248bffd9dcffa12d
kate <katesyrotchuk@gmail.com>
2016-09-28 15:07:47 +0300

    install aws ses

---
+++ src/AppBundle/AWS/ServiceSES.php
@@ -0,0 +1,65 @@
+<?php
+namespace AppBundle\Aws;
+
+use Aws\Ses\SesClient;
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\TwigBundle\TwigEngine;
+
+class ServiceSES
+{
+    /**
+     * @var SesClient
+     */
+    private $client;
+
+    /** @var TwigEngine  */
+    private $twig;
+
+    /** @var  string */
+    private $email_sender;
+
+    /** @var LoggerInterface  */
+    private $logger;
+
+    public function __construct($email_sender, SesClient $client, TwigEngine $twig, LoggerInterface $logger)
+    {
+        $this->email_sender = $email_sender;
+        $this->client = $client;
+        $this->twig = $twig;
+        $this->logger = $logger;
+    }
+
+    /**
+     * @param string $subject
+     * @param string $template
+     * @param array $options
+     * @param array $emails
+     */
+    public function sendEmail($emails, $subject, $template, $options)
+    {
+        try {
+            $this->client->sendEmail([
+                    'Source' => "Golos.ck.ua <" . $this->email_sender . ">",
+                    'Destination' => [
+                        'ToAddresses' => $emails
+                    ],
+                    'Message' => [
+                        'Subject' => [
+                            'Data' => $subject,
+                            'Charset' => 'UTF-8',
+                        ],
+                        'Body' => [
+                            'Html' => [
+                                'Data' => $this->twig->render(
+                                    $template, $options),
+                                'Charset' => 'UTF-8',
+                            ],
+                        ],
+                    ]]
+            );
+            $this->logger->info('Sending email was successful', ['to' => $emails, 'subject' => $subject]);
+        } catch (\Exception $e) {
+            $this->logger->critical('sending email was crashed', ['to' => $emails, 'subject' => $subject, 'e' => $e->getMessage()]);
+        }
+    }
+}
