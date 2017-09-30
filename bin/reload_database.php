d9fea830cca1b853a3024b1b9376998a4a7b8b4b
Ihor Sobchenko <sobchenko.ihor@gmail.com>
2017-09-30 22:38:58 +0300

    Symfony update from 2.7 to latest 3.3

--- bin/reload_database.php
+++ bin/reload_database.php
@@ -2,10 +2,10 @@
 
 require __DIR__ . '/base_script.php';
 
-show_run("Drop DB", "php app/console doctrine:database:drop --force");
-show_run("Create DB", "php app/console doctrine:database:create");
-show_run("Create scheme", "php app/console doctrine:schema:create");
-show_run("Install assets", "php app/console assets:install");
-show_run("Install assets", "php app/console assets:install web --symlink");
+show_run("Drop DB", "bin/console doctrine:database:drop --force");
+show_run("Create DB", "bin/console doctrine:database:create");
+show_run("Create scheme", "bin/console doctrine:schema:create");
+show_run("Install assets", "bin/console assets:install");
+show_run("Install assets", "bin/console assets:install web --symlink");
 
-show_run("Load fixtures", "php app/console doctrine:fixtures:load --no-interaction");
+show_run("Load fixtures", "bin/console doctrine:fixtures:load --no-interaction");
