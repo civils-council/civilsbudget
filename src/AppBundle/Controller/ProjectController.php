807e4376dc1c330cb2b47a12c81411cf4fabd277
smagolexandr <smagolexandr@gmail.com>
2017-10-24 11:45:24 +0300

    add new controller

--- src/AppBundle/Controller/ProjectController.php
+++ src/AppBundle/Controller/ProjectController.php
@@ -8,7 +8,6 @@ use AppBundle\Entity\VoteSettings;
 use AppBundle\Exception\ValidatorException;
 use AppBundle\Form\ProjectType;
 use AppBundle\Form\LikeProjectType;
-use Doctrine\Common\Collections\Expr\Expression;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
@@ -22,31 +21,6 @@ class ProjectController extends Controller
     const SERVER_ERROR                    = 'Server Error';
     const QUERY_CITY                      = 'city';
     const QUERY_PROJECT_ID                = 'project_id';
-
-    /**
-     * @Route("/categories", name="categories_list")
-     * @Template()
-     * @Method({"GET"})
-     */
-    public function categoriesListAction(Request $request)
-    {
-        $parameterBag = $request->query;
-        $em = $this->getDoctrine()->getManager();
-        $countAdminVoted = $em->getRepository(User::class)->findCountAdminVotedUsers($parameterBag);
-        $countVoted = $em->getRepository(User::class)->findCountVotedUsers($parameterBag);
-        $vote =  $em->getRepository(VoteSettings::class)->getProjectVoteSettingShow($request);
-        $voteCity =  $em->getRepository(VoteSettings::class)->getVoteSettingCities();
-        $votingList = $this->get('app.model.voting')->getVotingList();
-
-        return [
-            'debug' => true,
-            'countVoted' => $countVoted,
-            'countAdminVoted' => $countAdminVoted,
-            'voteSetting' => $vote,
-            'voteCity' => $voteCity,
-            "votings" => $votingList
-        ];
-    }
 
     /**
      * @Route("/{id}/projects", name="projects_list")
